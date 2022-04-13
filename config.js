4e455cfc58b0c20663a6821c241b40273f8b1c4c
youssefbotrouss2 <99335491+youssefbotrouss2@users.noreply.github.com>
2022-04-13 09:36:04 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
-//༼༺⋋[© 2022 Zim Bot Inc.]⋌༻༽\\
+//༼༺⋋[© 2022 YossefBot Inc.]⋌༻༽\\
 
 //~you can re-upload but tag my channel
 //or put my channel link in the description.
 //~you can recode or copy the codes but give
 //credit.
-//+27634090203 
+//+201289267791 
 //֎֎֎֎֎֎֎֎֎֎֎.
 
 //═══════[modules]════════\\
@@ -22,33 +22,33 @@ global.APIKeys = {
 }
 
 //═══════[modification]════════\\
-global.owner = ['27634090203'] //owner number, u can put multiple owner number, within quotations and seperated by comma.
-global.pemilik = ['27634090203'] //another owner number
-global.premium = ['27634090203'] //premium number
-global.pengguna = 'Drips' //username
-global.botnma = 'ZIM BOT INC' //bot name
-global.ownernma = 'Drips' //owner name
-global.packname = 'Drips' //sticker package name
-global.author = 'By Drips Memes' //sticker author name
+global.owner = ['201289267791'] //owner number, u can put multiple owner number, within quotations and seperated by comma.
+global.pemilik = ['201289267791'] //another owner number
+global.premium = ['201289267791'] //premium number
+global.pengguna = 'Yossef' //username
+global.botnma = 'YossefBOT INC' //bot name
+global.ownernma = 'Yossef' //owner name
+global.packname = 'Yossef' //sticker package name
+global.author = 'By Yossef Memes' //sticker author name
 global.sessionName = 'session' //session name
 global.prefa = ['#','!','/',''] //prefix
 global.sp = '🔵' //design
 global.mess = {
     success: 'Takaenda Done!',
     admin: 'you must be admin to use this cmd!',
-    botAdmin: 'zim bot must be admin neh!',
-    owner: 'This cmd is for zim bot owner only',
+    botAdmin: 'Yossefbot must be admin neh!',
+    owner: 'This cmd is for Yossefbot owner only',
     group: 'oooh sorry this cmd is for groups only!',
     private: 'goossssh it can only used in dms!',
     bot: 'This cmd  its only for the bot host',
-    wait: 'wait zim bot processing ',
+    wait: 'wait Yossefbot processing ',
     endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
 }
 global.limitawal = {
     premium: "Infinity", //premium user limit
     free: 100000 //free user limit
 }
-global.thumb = fs.readFileSync('./Zimbot/drips.jpg')
+global.thumb = fs.readFileSync('./Zimbot/Yossef.jpg')
 
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
